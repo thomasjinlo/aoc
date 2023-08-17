4af507e0bf9859c62c667ff2257ca649310edaa7
Thomas Lo <thomasjinlo@gmail.com>
2023-08-16 20:35:43 -0700

    day 2 solution part 2

--- day2/main.go
+++ day2/main.go
@@ -17,35 +17,57 @@ func main() {
         elfRock = 65
         elfPaper = 66
         elfScissor = 67
-        myRock   = 88
-        myPaper = 89
-        myScissor = 90
+        shouldLose   = 88
+        shouldDraw = 89
+        shouldWin = 90
     )
 
     totalScore := 0
+    // calculateScore := map[uint8]map[uint8]int{
+    //     elfRock: {
+    //         myRock: 3,
+    //         myPaper: 6,
+    //         myScissor: 0,
+    //     },
+    //     elfPaper: {
+    //         myRock: 0,
+    //         myPaper: 3,
+    //         myScissor: 6,
+    //     },
+    //     elfScissor: {
+    //         myRock: 6,
+    //         myPaper: 0,
+    //         myScissor: 3,
+    //     },
+    // }
+    // calculateFreeScore := map[uint8]int{
+    //     myRock: 1,
+    //     myPaper: 2,
+    //     myScissor: 3,
+    // }
+
     calculateScore := map[uint8]map[uint8]int{
         elfRock: {
-            myRock: 3,
-            myPaper: 6,
-            myScissor: 0,
+            shouldLose: 3,
+            shouldDraw: 1,
+            shouldWin: 2,
         },
         elfPaper: {
-            myRock: 0,
-            myPaper: 3,
-            myScissor: 6,
+            shouldLose: 1,
+            shouldDraw: 2,
+            shouldWin: 3,
         },
         elfScissor: {
-            myRock: 6,
-            myPaper: 0,
-            myScissor: 3,
+            shouldLose: 2,
+            shouldDraw: 3,
+            shouldWin: 1,
         },
     }
     calculateFreeScore := map[uint8]int{
-        myRock: 1,
-        myPaper: 2,
-        myScissor: 3,
+        shouldLose: 0,
+        shouldDraw: 3,
+        shouldWin: 6,
     }
-
     for bufScanner := bufio.NewScanner(input); bufScanner.Scan(); {
         moves := bufScanner.Text()
         elfMove, myMove := moves[0], moves[2]
